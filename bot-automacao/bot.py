cd5304054021707bd6950c4c9f0e0600d4eb52f9
brukyu <bbrunoigorr@gmail.com>
2023-02-23 21:57:15 -0300

    code refactoring

--- bot-automacao/bot.py
+++ bot-automacao/bot.py
@@ -1,52 +1,83 @@
-from typing import KeysView
 from botcity.core import DesktopBot
 
 class Bot(DesktopBot):
-    def action(self, execution=None):
-        # Executando a aplicação Small Start
-        self.execute(r'C:\Users\Public\Desktop\Small Start.lnk')
 
-        # Preenchendo a senha
-        if not self.find("senha", matching=0.97, waiting_time=10000):
-            self.not_found("senha")
+    # Nomes descritivos para valores literais
+    PASSWORD_FIELD = "senha"
+    STOCK_OPTION = "estoque"
+    ICMS_OPTION = "icms"
+    MERCADORIA_OPTION = "mercadoria"
+    CFOP_OPTION = "cfop"
+    OK_BUTTON = "ok"
+    MAX_MATCHING = 0.97
+    WAITING_TIME = 10000
+    CFOP_RELATIVE_POSITION = (121, 13)
+
+    def action(self, execution=None):
+        self.open_small_start()
+        self.fill_password()
+        self.go_to_stock_screen()
+        self.select_icms_option()
+        self.select_mercadoria_option()
+        self.select_mercadoria()
+        self.select_origem()
+        self.select_cscson()
+        self.select_cfop()
+        self.click_ok_button()
+
+    def open_small_start(self):
+        """Executa a aplicação Small Start."""
+        self.execute(r"C:\Users\Public\Desktop\Small Start.lnk")
+
+    def fill_password(self):
+        """Preenche a senha."""
+        if not self.find(self.PASSWORD_FIELD, matching=self.MAX_MATCHING, waiting_time=self.WAITING_TIME):
+            self.not_found(self.PASSWORD_FIELD)
         self.paste("1234")
         self.enter()
 
-        # Navegando para a tela de estoque
-        if not self.find("estoque", matching=0.97, waiting_time=10000):
-            self.not_found("estoque")
+    def go_to_stock_screen(self):
+        """Navega para a tela de estoque."""
+        if not self.find(self.STOCK_OPTION, matching=self.MAX_MATCHING, waiting_time=self.WAITING_TIME):
+            self.not_found(self.STOCK_OPTION)
         self.click()
 
-        # Selecionando o ICMS
-        if not self.find("icms", matching=0.97, waiting_time=10000):
-            self.not_found("icms")
+    def select_icms_option(self):
+        """Seleciona o ICMS."""
+        if not self.find(self.ICMS_OPTION, matching=self.MAX_MATCHING, waiting_time=self.WAITING_TIME):
+            self.not_found(self.ICMS_OPTION)
         self.click()
 
-        # Selecionando a Mercadoria
-        if not self.find("mercadoria", matching=0.97, waiting_time=10000):
-            self.not_found("mercadoria")
-        self.click(),
+    def select_mercadoria_option(self):
+        """Seleciona a opção Mercadoria."""
+        if not self.find(self.MERCADORIA_OPTION, matching=self.MAX_MATCHING, waiting_time=self.WAITING_TIME):
+            self.not_found(self.MERCADORIA_OPTION)
+        self.click()
 
-        # Selecionando Mercadoria
+    def select_mercadoria(self):
+        """Seleciona Mercadoria."""
         self.type_down()
         self.type_down()
         self.enter()
 
-        # Selecionando Origem
-        self.tab()
+    def select_origem(self):
+        """Seleciona Origem."""
+        self.tab()  
         self.tab()
         self.tab()
         self.tab()
         self.type_down()
         self.type_down()
 
-        # Selecionando CSCSON
+    def select_cscson(self):
+        """Seleciona CSCSON."""
         self.tab()
         self.type_down()
         self.type_down()
         self.type_down()
 
-        # Selecionando CFOP
+    def select_cfop(self):
+        """Seleciona CFOP."""
         if not self.find("cfop", matching=0.97, waiting_time=10000):
             self.not_found("cfop")
         self.click_relative(121, 13)
@@ -59,16 +90,24 @@ class Bot(DesktopBot):
         self.type_down()
         self.enter()
 
-        # Selecionando CSCSON NFC-e
+        #Seleciona CSCSON para NFC-e."""
         self.type_down()
         self.type_down()
         self.type_down()
         self.type_down()
         self.type_down()
         self.type_down()
+    
+    def click_ok_button(self):
+        """Clica no botão OK após preencher os dados."""
         if not self.find("ok", matching=0.97, waiting_time=10000):
             self.not_found("ok")
         self.click()
+
+
+
+def not_found(self, label):
+    print(f"Element not found: {label}")
 
     def not_found(self, label):
         print(f"Element not found: {label}")
